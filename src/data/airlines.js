bea5956d9301bc9c6241aaaac776b5a4c3f0009d
hachrisjordan <nguyen77@miamioh.edu>
2024-12-05 09:48:13 -0500

    data

---
+++ src/data/airlines.js
@@ -0,0 +1,37 @@
+import UA_miles from './UA_miles.json';
+import SQ_miles from './SQ_miles.json';
+import LH_miles from './LH_miles.json';
+import LX_miles from './LX_miles.json';
+import TG_miles from './TG_miles.json';
+import BR_miles from './BR_miles.json';
+import NH_miles from './NH_miles.json';
+import OS_miles from './OS_miles.json';
+import OZ_miles from './OZ_miles.json';
+import NZ_miles from './NZ_miles.json';
+import SA_miles from './SA_miles.json';
+import AI_miles from './AI_miles.json';
+import LO_miles from './LO_miles.json';
+import SN_miles from './SN_miles.json';
+import A3_miles from './A3_miles.json';
+import TP_miles from './TP_miles.json';
+import MS_miles from './MS_miles.json';
+import WY_miles from './WY_miles.json';
+import GF_miles from './GF_miles.json';
+import AC_miles from './AC_miles.json';
+import ET_miles from './ET_miles.json';
+import OU_miles from './OU_miles.json';
+import ZH_miles from './ZH_miles.json';
+import TK_miles from './TK_miles.json';
+import AV_miles from './AV_miles.json';
+import CM_miles from './CM_miles.json';
+import YN_miles from './YN_miles.json';
+import EN_miles from './EN_miles.json';
+
+export {
+  UA_miles, SQ_miles, LH_miles, LX_miles, TG_miles,
+  BR_miles, NH_miles, OS_miles, OZ_miles, NZ_miles,
+  SA_miles, AI_miles, LO_miles, SN_miles, A3_miles,
+  TP_miles, MS_miles, WY_miles, GF_miles, AC_miles,
+  ET_miles, OU_miles, ZH_miles, TK_miles, AV_miles,
+  CM_miles, YN_miles, EN_miles
+}; 
